436fe55958f410bcd24148d827ddc3bc99bd3f64
ShowTimeU <d.yankovych@gmail.com>
2020-05-06 18:50:35 +0300

    Update food-list.component.ts

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
-export class FoodListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class FoodListComponent {
 
 }
