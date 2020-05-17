2f6eaeeda811d202261d057c5bcfc62a587948d8
ShowTimeU <d.yankovych@gmail.com>
2020-05-17 20:09:56 +0300

    Create ProductCreationResponse.java

---
+++ src/main/java/com/dontwaste/model/admin/response/ProductCreationResponse.java
@@ -0,0 +1,22 @@
+package com.dontwaste.model.admin.response;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Builder
+public class ProductCreationResponse {
+
+    private Long id;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+    private String productName;
+    private String description;
+    private String image;
+    private Double startingPrice;
+    private Double price;
+
+
+}
