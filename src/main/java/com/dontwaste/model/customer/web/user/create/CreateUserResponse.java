7bd541120e02a10f2674a44972100ad7cb8315f9
ShowTimeU <d.yankovych@gmail.com>
2020-05-17 20:10:06 +0300

    Create CreateUserResponse.java

---
+++ src/main/java/com/dontwaste/model/customer/web/user/create/CreateUserResponse.java
@@ -0,0 +1,16 @@
+package com.dontwaste.model.customer.web.user.create;
+
+import java.time.LocalDateTime;
+
+public class CreateUserResponse {
+
+    private Long id;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String area;
+
+}
