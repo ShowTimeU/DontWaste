6466f897a2a99b70f2d9bac9f32a453ee7223c81
ShowTimeU <d.yankovych@gmail.com>
2020-05-17 20:09:48 +0300

    Create ProductController.java

---
+++ src/main/java/com/dontwaste/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.dontwaste.controller;
+
+
+import com.dontwaste.model.admin.request.ProductCreationRequest;
+import com.dontwaste.model.admin.response.ProductCreationResponse;
+import com.dontwaste.model.customer.entity.ProductEntity;
+import com.dontwaste.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping(value = "/createProduct")
+    public ProductCreationResponse createProduct(@RequestBody ProductCreationRequest productCreationRequest){
+        return productService.createProduct(productCreationRequest);
+    }
+
+    @DeleteMapping(value = "deleteProduct/{id}")
+    public void deleteProduct(@PathVariable("id") Long id){
+        productService.deleteProduct(id);
+    }
+
+}
