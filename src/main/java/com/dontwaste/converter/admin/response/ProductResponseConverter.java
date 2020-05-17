1a208fcc1c4a70bc21ab3f93c74402f655ae26e5
ShowTimeU <d.yankovych@gmail.com>
2020-05-17 20:09:50 +0300

    Create ProductResponseConverter.java

---
+++ src/main/java/com/dontwaste/converter/admin/response/ProductResponseConverter.java
@@ -0,0 +1,22 @@
+package com.dontwaste.converter.admin.response;
+
+import com.dontwaste.model.admin.response.ProductCreationResponse;
+import com.dontwaste.model.customer.entity.ProductEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductResponseConverter {
+
+    public ProductCreationResponse convertProductToWeb(ProductEntity productEntity){
+        return ProductCreationResponse.builder()
+                .id(productEntity.getId())
+                .createdOn(productEntity.getCreatedOn())
+                .updatedOn(productEntity.getUpdatedOn())
+                .productName(productEntity.getProductName())
+                .description(productEntity.getDescription())
+                .image(productEntity.getImage())
+                .startingPrice(productEntity.getStartingPrice())
+                .price(productEntity.getPrice())
+                .build();
+    }
+}
