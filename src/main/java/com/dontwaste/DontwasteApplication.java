d724ba9e35b83b9d78cb42795e8977ead0164da7
ShowTimeU <d.yankovych@gmail.com>
2020-05-17 20:09:53 +0300

    Create DontwasteApplication.java

---
+++ src/main/java/com/dontwaste/DontwasteApplication.java
@@ -0,0 +1,13 @@
+package com.dontwaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontwasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DontwasteApplication.class, args);
+    }
+
+}
