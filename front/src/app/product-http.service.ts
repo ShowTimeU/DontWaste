5fa4a25978d2c4ce16f72a50614e7e30e54eb89d
ShowTimeU <d.yankovych@gmail.com>
2020-05-18 22:36:52 +0300

    Update product-http.service.ts

--- front/src/app/product-http.service.ts
+++ front/src/app/product-http.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Product} from './model/product';
+import {User} from "./model/user";
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class ProductHTTPService {
 
   createProduct(body): Observable<Product> {
     return this.httpClient.post<Product>('/api/createProduct', body);
+  }
+
+  createUser(body): Observable<User> {
+    return this.httpClient.post<User>('/api/createUser', body);
   }
 }
