d623c31760ba5a290dbb6a80891c474ebc17d573
ShowTimeU <d.yankovych@gmail.com>
2020-05-20 23:14:20 +0300

    Update ProductResponseConverter.java

--- src/main/java/com/dontwaste/converter/admin/response/ProductResponseConverter.java
+++ src/main/java/com/dontwaste/converter/admin/response/ProductResponseConverter.java
@@ -19,9 +19,9 @@ public class ProductResponseConverter {
                 .price(productEntity.getPrice())
                 .area(productEntity.getArea())
                 .institution(productEntity.getInstitution())
-                .kosher(productEntity.isKosher())
-                .vegan(productEntity.isVegan())
-                .vegeterian(productEntity.isVegeterian())
+//                .kosher(productEntity.isKosher())
+//                .vegan(productEntity.isVegan())
+//                .vegeterian(productEntity.isVegeterian())
                 .build();
     }
 }
