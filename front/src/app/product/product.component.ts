05c8ad2df61a5bee46c6280cdbb33023a8989c65
ShowTimeU <d.yankovych@gmail.com>
2020-05-20 23:14:16 +0300

    Update product.component.ts

--- front/src/app/product/product.component.ts
+++ front/src/app/product/product.component.ts
@@ -3,15 +3,31 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Product} from '../model/product';
 import {ProductHTTPService} from '../product-http.service';
 
+interface Area {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
+
 export class ProductComponent implements OnInit {
   formGroup: FormGroup;
   createdProduct: Product;
   id: number;
+  selectedValue: string;
+  areas: Area[] = [
+    {value: 'Jerusalem', viewValue: 'Jerusalem District'},
+    {value: 'North', viewValue: 'Northern District'},
+    {value: 'Haifa', viewValue: 'Haifa District'},
+    {value: 'Centre', viewValue: 'Central District'},
+    {value: 'Tel Aviv', viewValue: 'Tel Aviv District'},
+    {value: 'South', viewValue: 'Southern District'},
+    {value: 'Judea and Samaria Area', viewValue: 'Judea and Samaria Area'}
+  ];
 
   constructor(private fb: FormBuilder,
               private productHTTPService: ProductHTTPService
@@ -27,7 +43,9 @@ export class ProductComponent implements OnInit {
       description: new FormControl('', Validators.required),
       image: new FormControl('', Validators.required),
       startingPrice: new FormControl('', Validators.required),
-      price: new FormControl('', Validators.required)
+      price: new FormControl('', Validators.required),
+      area: new FormControl('', Validators.required),
+      institution: new FormControl('', Validators.required)
     });
   }
 
