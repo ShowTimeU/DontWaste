21e2c43cfb6af9ecca57c9b3a96caccc588ae8a9
ShowTimeU <d.yankovych@gmail.com>
2020-05-24 22:56:21 +0300

    Update app.component.ts

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {User} from "./model/user";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "./services/authentication.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentUser: User;
   title = 'dontwaste';
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+
 }
