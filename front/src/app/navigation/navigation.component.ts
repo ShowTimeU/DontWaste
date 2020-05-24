b13411c5d216e81b642f1688911c79782614da2d
ShowTimeU <d.yankovych@gmail.com>
2020-05-24 22:56:38 +0300

    Update navigation.component.ts

--- front/src/app/navigation/navigation.component.ts
+++ front/src/app/navigation/navigation.component.ts
@@ -2,6 +2,8 @@ import {Component, ViewChild} from '@angular/core';
 import {MatSidenav} from '@angular/material/sidenav';
 import {MatDialog} from '@angular/material/dialog';
 import {LoginPageComponent} from '../login-page/login-page.component';
+import {AuthenticationService} from "../services/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navigation',
@@ -19,11 +21,19 @@ export class NavigationComponent {
     this.sidenav.close();
   }
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog,
+              private authenticationService: AuthenticationService,
+              private router: Router) {}
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/']);
+  }
 
   openDialog() {
     this.dialog.open(LoginPageComponent, {
       width: '400px'
     });
   }
+
 }
