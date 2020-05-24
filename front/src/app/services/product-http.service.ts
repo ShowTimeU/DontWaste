8eefd65771cac1fac31a524be9328e34df4c7f2f
ShowTimeU <d.yankovych@gmail.com>
2020-05-24 22:56:52 +0300

    Create product-http.service.ts

---
+++ front/src/app/services/product-http.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Product} from '../model/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductHTTPService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  createProduct(body): Observable<Product> {
+    return this.httpClient.post<Product>('/api/createProduct', body);
+  }
+
+  getAllProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>('/api/getAllProducts');
+  }
+
+  getProductsByArea(areaName): Observable<Product[]> {
+    let params = new HttpParams();
+    params = params.set('area', areaName);
+    return this.httpClient.get<Product[]>('/api/getProductsByArea', {params: params});
+  }
+
+}
