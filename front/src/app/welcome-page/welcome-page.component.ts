afb82d470e3bd7816430a8c995462adc5f54e356
ShowTimeU <d.yankovych@gmail.com>
2020-05-26 23:33:57 +0300

    Update welcome-page.component.ts

--- front/src/app/welcome-page/welcome-page.component.ts
+++ front/src/app/welcome-page/welcome-page.component.ts
@@ -1,30 +1,20 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductHTTPService} from "../services/product-http.service";
 import {Product} from "../model/product";
-import {Subscription} from "rxjs";
-import {UserHttpService} from "../services/user-http.service";
-import {AuthenticationService} from "../services/authentication.service";
-import {User} from "../model/user";
+
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.css']
 })
-export class WelcomePageComponent implements OnInit, OnDestroy {
+export class WelcomePageComponent implements OnInit {
 
-  currentUserSubscription: Subscription;
-  currentUser: User;
   public products: Product[] = [];
 
-  constructor(private productHTTPService: ProductHTTPService,
-              private authenticationService: AuthenticationService,
-              private userService: UserHttpService) {
-    this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
-      this.currentUser = user;
-    });
-  }
-  ngOnInit(): void {
+  constructor(private productHTTPService: ProductHTTPService) { }
+
+  ngOnInit() {
     this.getAllProducts();
   }
 
@@ -32,7 +22,4 @@ export class WelcomePageComponent implements OnInit, OnDestroy {
     return this.productHTTPService.getAllProducts().subscribe(data => this.products = data);
   }
 
-  ngOnDestroy() {
-    this.currentUserSubscription.unsubscribe();
-  }
 }
